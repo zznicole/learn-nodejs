62d7dba9f706199d70b793ee6ba81712b0ebddc9
Nicole Zheng <zznicole@hotmail.com>
2021-01-29 23:56:16 +0100

    2. export and fetch multiple modules from one file

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-// 1. getting started with node.js
-console.log('Hello to Nodejs from Nic');
+// // getting started with node.js
+// console.log('Hello to Nodejs from Nic');
 
-// 2. fetching a single module from a file
+// // 1. fetching a single module from a file
+// const mathModules = require('./mathModules');
+// console.log(mathModules);
+// console.log(mathModules(9, 9));
+
+// 2. fetching multiple moduels from a file
+const { MathObjects } = require('./mathModules');
 const mathModules = require('./mathModules');
-console.log(mathModules);
-console.log(mathModules(9, 9));
+console.log(mathModules.addition(2,7));
+console.log(mathModules.PI);
+console.log(new MathObjects());
--- mathModules.js
+++ mathModules.js
@@ -1,6 +1,15 @@
-const addition = (num1, num2) => {
-  console.log(num1 + num2);
-}
+const { isModuleDeclaration } = require("@babel/types");
 
-// exporting single module from a file 
-module.exports = addition;
+const addition = (num1, num2) => num1 + num2;
+const PI = 3.14;
+class MathObjects{
+  constructor(){
+    console.log('object is created');
+  }
+};
+
+// // * exporting single module from a file 
+// module.exports = addition;
+
+// * exporting multiple modules from the same file
+module.exports = {addition: addition, PI: PI, MathObjects: MathObjects}
