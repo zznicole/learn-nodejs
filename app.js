c3266d190d61550da93d77a0f67b193249325cd9
Nicole Zheng <zznicole@hotmail.com>
2021-01-29 15:21:49 +0100

    1. fetch and export  a single module from a file

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+// 1. getting started with node.js
 console.log('Hello to Nodejs from Nic');
+
+// 2. fetching a single module from a file
+const mathModules = require('./mathModules');
+console.log(mathModules);
+console.log(mathModules(9, 9));
---
+++ mathModules.js
@@ -0,0 +1,6 @@
+const addition = (num1, num2) => {
+  console.log(num1 + num2);
+}
+
+// exporting single module from a file 
+module.exports = addition;
