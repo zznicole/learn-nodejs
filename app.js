4cb687a241767adbb62ad2da317a3aa7ff4f1594
Nicole Zheng <zznicole@hotmail.com>
2021-01-31 22:13:46 +0100

    4. how to use the benefit of EventEmitter to create customized objects

--- app.js
+++ app.js
@@ -20,4 +20,23 @@ const eventEmitter = new EventEmitter();
 eventEmitter.on('mathModules', (num1, num2) => console.log(num1 + num2) );
 eventEmitter.emit('mathModules',1,2);
 
+// 4. how to use the benefit of EventEmitter to create customized objects
+class Person extends EventEmitter{
+  constructor(name){
+    super();
+    this._name = name;
+  }
 
+  get name(){
+    return this._name;
+  }
+}
+
+let nic = new Person('Nic');
+let emma = new Person('Emma');
+
+nic.on('name', ()=> console.log('Hi, my nam is ' + nic.name + ','));
+emma.on('name', ()=> console.log('and this is my friend ' + emma.name + '.'));
+// it works sychronously when emitting an event to occure 
+nic.emit('name');
+emma.emit('name');
