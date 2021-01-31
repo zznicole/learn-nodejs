76f716407d193b30ceabc3a53e48daaf478642f5
Nicole Zheng <zznicole@hotmail.com>
2021-01-31 22:11:17 +0100

    3. how to work with EventEmitter

--- app.js
+++ app.js
@@ -6,9 +6,18 @@
 // console.log(mathModules);
 // console.log(mathModules(9, 9));
 
-// 2. fetching multiple moduels from a file
-const { MathObjects } = require('./mathModules');
-const mathModules = require('./mathModules');
-console.log(mathModules.addition(2,7));
-console.log(mathModules.PI);
-console.log(new MathObjects());
+// // 2. fetching multiple moduels from a file
+// const { MathObjects } = require('./mathModules');
+// const mathModules = require('./mathModules');
+// console.log(mathModules.addition(2,7));
+// console.log(mathModules.PI);
+// console.log(new MathObjects());
+
+// 3. how to work with EventEmitter
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('mathModules', (num1, num2) => console.log(num1 + num2) );
+eventEmitter.emit('mathModules',1,2);
+
+
